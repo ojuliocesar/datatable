4fc5afe431df0ee02507db2a8b8b9855eee0bdce
ojuliocesar <97842753+ojuliocesar@users.noreply.github.com>
2022-10-18 21:27:09 -0300

    Finished PHPMailer

--- backend/activeUser.php
+++ backend/activeUser.php
@@ -1,18 +1,49 @@
 <?php
 
-include ('includex/conexao.php');
+include 'includes/conexao.php';
 
 try {
 
-    $token = $_GET['token'];
+  $token = $_GET['token'];
 
-    $sql = "
-        UPDATE user u 
-            INNER JOIN user_token t ON t.id_user = u.id 
-            SET u.status = 1 
-        WHERE t.token = '$token'
-    ";
+  $sql = "UPDATE user u INNER JOIN user_token t ON t.id_user = u.id SET u.status = 1 WHERE t.token = '$token'";
 
-} catch (PDOException $e) {
-    echo $e->getMessage();
+  $comando = $conn->prepare($sql);
+
+  $comando->execute();
+
+  $return = $comando->rowCount();
+} catch (PDOException $erro) {
+
+  $return = 0;
 }
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Sistema Senac - Ativação do Usuário</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+</head>
+
+<body>
+  <div class="container-sm">
+
+    <div class="alert <?php echo $retorno == true ? 'alert-success' : 'alert-danger' ?>" role="alert">
+      <?php echo $return != 0 ? 'Cadastro ativado com sucesso!' : 'Erro ao ativar cadastro' ?>
+    </div>
+
+    <a href="./../index.html">
+        <button type="button" class="btn btn-primary">Acessar Sistema</button>
+    </a>
+
+  </div>
+</body>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+
+</html>
